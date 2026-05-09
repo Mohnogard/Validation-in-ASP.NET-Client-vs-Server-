17f960abdc7ce3914d602c01955b0ae770998504
mohish <mohammed.az.ishaqat@gmail.com>
2026-05-09 18:48:55 +0300

    Initial commit

---
+++ .Net/Tasks(MVC)_1/Database Connectivity & CRUD Operations/Database Connectivity & CRUD Operations/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Database_Connectivity___CRUD_Operations.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database_Connectivity___CRUD_Operations.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ .Net/Tasks(MVC)_1/Database Connectivity & CRUD Operations/Database Connectivity & CRUD Operations/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database_Connectivity___CRUD_Operations.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(0, 100)]
+        public int Age { get; set; }
+    }
+}
